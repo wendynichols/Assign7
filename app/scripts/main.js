966d9a6bd75f48ff1bfef46749c3da32c897ee8b
Wendy Nichols <wendlelyn@gmail.com>
2014-10-06 11:38:23 -0400

    changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,25 @@
 
 
-$(function(){
 
-  function animation(elem,options){
-    elem.stop().animate(options,{
-    duration: 1000,
-    specialEasing: {
-      width: "linear",
-      height: "easeOutBounce"
-    }},200);
-  }
+$("h1").click( function() {
+  $('div.content').addClass('open');
 
-  $(".accordion").on("click",".section",function(e){
-      e.preventDefault();
-      var current = $(this).next(".content");
-    animation($(this).parent().children(".content").not(current),{
-      "height":"0px"
-    });
-    animation(current,{
-      "height":"100px"
-    });
-  });
+});
+
+
+
+$('h1.one').click( function() {
+  $('div.two').addClass('off').removeClass('on');
+  $('div.three').addClass('off').removeClass('on');
+  $('div.one').removeClass('off').addClass('on');
+});
+$('h1.two').click( function() {
+  $('div.three').addClass('off').removeClass('on');
+  $('div.one').addClass('off').removeClass('on');
+  $('div.two').removeClass('off').addClass('on');
+});
+$('h1.three').click( function() {
+  $('div.one').addClass('off').removeClass('on');
+  $('div.two').addClass('off').removeClass('on');
+  $('div.three').removeClass('off').addClass('on');
 });
