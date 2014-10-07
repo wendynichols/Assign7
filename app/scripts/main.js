3db9642d977f9db29cf52561fdb9c1f5672f4a2d
Wendy Nichols <wendlelyn@gmail.com>
2014-10-06 21:56:36 -0400

    changed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,23 @@
 
+$(function(){
 
+  function animation(elem,options){
+    elem.stop().animate(options,{
+    duration: 1000,
+    specialEasing: {
+      width: "linear",
+      height: "easeOutBounce"
+    }},200);
+  }
 
-$("h1").click( function() {
-  $('div.content').addClass('open');
-
-});
-
-
-
-$('h1.one').click( function() {
-  $('div.two').addClass('off').removeClass('on');
-  $('div.three').addClass('off').removeClass('on');
-  $('div.one').removeClass('off').addClass('on');
-});
-$('h1.two').click( function() {
-  $('div.three').addClass('off').removeClass('on');
-  $('div.one').addClass('off').removeClass('on');
-  $('div.two').removeClass('off').addClass('on');
-});
-$('h1.three').click( function() {
-  $('div.one').addClass('off').removeClass('on');
-  $('div.two').addClass('off').removeClass('on');
-  $('div.three').removeClass('off').addClass('on');
+  $(".accordion").on("click",".section",function(e){
+      e.preventDefault();
+      var current = $(this).next(".content");
+    animation($(this).parent().children(".content").not(current),{
+      "height":"0px"
+    });
+    animation(current,{
+      "height":"100px"
+    });
+  });
 });
