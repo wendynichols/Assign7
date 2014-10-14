0f124c916644ee257c3178218e29eeaf56bc4a2f
Wendy Nichols <wendlelyn@gmail.com>
2014-10-14 19:40:51 -0400

    changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,12 @@
 
-$(function(){
+// $('.moving').click(function() {
+//   console.log('click');
+//    $('section').removeClass('moving');
+//    $(this).addClass('section');
+// });
 
-  function animation(elem,options){
-    elem.stop().animate(options,{
-    duration: 1000,
-    specialEasing: {
-      width: "linear",
-      height: "easeOutBounce"
-    }},200);
-  }
-
-  $(".accordion").on("click",".section",function(e){
-      e.preventDefault();
-      var current = $(this).next(".content");
-    animation($(this).parent().children(".content").not(current),{
-      "height":"0px"
-    });
-    animation(current,{
-      "height":"100px"
-    });
-  });
+$('h2').on('click',function(){
+  $('span').addClass('hide');
+  $(this).next().find('span').removeClass ('hide');
+  $(this).next().find('span').addClass('appear');
 });
