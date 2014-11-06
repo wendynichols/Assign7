89cfbb48ed1a2da41c637867b67bf7c64d374ad5
Wendy Nichols <wendlelyn@gmail.com>
2014-11-06 14:49:37 -0500

    updated

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,5 +1,20 @@
 
-$("section").click(function(){
-  $("section").removeClass("sec1")
-  $(this).addClass("sec1");
-});
+$('.content2').css("display","none");
+$('.content3').css("display","none");
+
+
+$('.header1').click(function(){
+  $('.content1').css("display","block");
+  $('.content2').css("display","none");
+  $('.content3').css("display","none");
+    });
+$('.header2').click(function(){
+  $('.content2').css("display","block");
+  $('.content1').css("display","none");
+  $('.content3').css("display","none");
+    });
+$('.header3').click(function(){
+  $('.content3').css("display","block");
+  $('.content1').css("display","none");
+  $('.content2').css("display","none");
+    });
